84904933530c7c2090f51045face4cdfee98b6e8
Sree-bala <117964032+Sree-bala@users.noreply.github.com>
2025-03-13 12:45:27 +0530

    New change backend before commit

--- models/User.js
+++ models/User.js
@@ -5,7 +5,8 @@ const ExpenseSchema = new mongoose.Schema({
 });
 
 const UserSchema = new mongoose.Schema({
-  username: { type: String, required: true },
+  firstName: { type: String, required: true },
+  lastName: { type: String, required: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
   income: { type: Number, required: true, default: 0 },
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -15,6 +15,7 @@ router.post('/register', async (req, res) => {
     await user.save();
     res.status(201).json({ message: 'User registered successfully' });
   } catch (error) {
+    console.log("data",error);
     res.status(500).json({ error: error.message });
   }
 });
@@ -24,7 +25,7 @@ router.post('/login', async (req, res) => {
     const { email, password } = req.body;
     console.log("data",req.body);
     const user = await User.findOne({ email });
-    if (!user) return res.status(400).json({ message: 'User not found' });
+    if (!user) return res.status(404).json({ message: 'User not found' });
     const isMatch = await bcrypt.compare(password, user.password);
     if (!isMatch) return res.status(400).json({ message: 'Invalid credentials' });
     const token = jwt.sign({ id: user._id }, "mysecret", { expiresIn: '12h' });
